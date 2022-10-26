ea6e1ac63abf65c0783f289dbbc2701dda1713f3
kornelM <makusz07@gmail.com>
2022-10-26 19:08:02 +0200

    Started implementing SingleWordSearchServiceImpl

--- text-analyzer-core/src/main/java/com/text/analyzer/service/SingleWordSearchService.java
+++ text-analyzer-core/src/main/java/com/text/analyzer/service/SingleWordSearchService.java
@@ -1,4 +1,10 @@
 package com.text.analyzer.service;
 
+import com.text.analyzer.dto.SingleWordSearchDto;
+
+import java.util.List;
+
 public interface SingleWordSearchService {
+
+    SingleWordSearchDto processSearches(List<String> searches);
 }
