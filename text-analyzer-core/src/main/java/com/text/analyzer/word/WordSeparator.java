acf971113654ee16f6e79752a71b2727b6bd1537
kornelM <makusz07@gmail.com>
2022-11-15 23:24:08 +0100

    Refactored WordSeparator class to use stream instead of for loop

--- text-analyzer-core/src/main/java/com/text/analyzer/word/WordSeparator.java
+++ text-analyzer-core/src/main/java/com/text/analyzer/word/WordSeparator.java
@@ -1,42 +1,28 @@
 package com.text.analyzer.word;
 
 import com.text.analyzer.pojo.SearchType;
+import lombok.experimental.UtilityClass;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import static java.util.Objects.isNull;
+import static java.util.stream.Collectors.toList;
 
+@UtilityClass
 public class WordSeparator {
 
     public static final String STRING_SPACE = " ";
 
     public static Map<SearchType, List<String>> separate(List<String> searches) {
-        Map<SearchType, List<String>> separatedSearches = new HashMap<>();
-
-        List<String> single = new ArrayList<>();
-        List<String> multi = new ArrayList<>();
-
-        for (String search : searches) {
-            String trimmedSearch = search.trim();
-            if (trimmedSearch.split(STRING_SPACE).length > 1) {
-                if (isNull(separatedSearches.get(SearchType.MULTI))) {
-                    multi.add(trimmedSearch);
-                    separatedSearches.put(SearchType.MULTI, multi);
-                } else {
-                    separatedSearches.get(SearchType.MULTI).add(trimmedSearch);
-                }
-            } else {
-                if (isNull(separatedSearches.get(SearchType.SINGLE))) {
-                    single.add(trimmedSearch);
-                    separatedSearches.put(SearchType.SINGLE, single);
-                } else {
-                    separatedSearches.get(SearchType.SINGLE).add(trimmedSearch);
-                }
-            }
-        }
-        return separatedSearches;
+        return searches.stream()
+                .map(String::trim)
+                .collect(Collectors.groupingBy(
+                                s -> s.split(STRING_SPACE).length > 1 ? SearchType.MULTI : SearchType.SINGLE,
+                                HashMap::new,
+                                toList()
+                        )
+                );
     }
 }
---
+++ text-analyzer-core/src/test/java/com/text/analyzer/word/WordSeparatorTest.java
@@ -0,0 +1,39 @@
+package com.text.analyzer.word;
+
+import com.text.analyzer.pojo.SearchType;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WordSeparatorTest {
+
+    @Test
+    public void test() {
+        //given
+        String single = "single ";
+        String singleTwo = " single_two";
+        String multiOne = "multi one";
+        String multiTwentyOne = "multi twenty one";
+
+        List<String> given = List.of(
+                single,
+                singleTwo,
+                multiOne,
+                multiTwentyOne
+        );
+
+        Map<SearchType, List<String>> expected = Map.of(
+                SearchType.SINGLE, List.of(single.trim(), singleTwo.trim()),
+                SearchType.MULTI, List.of(multiOne, multiTwentyOne)
+        );
+
+        //when
+        Map<SearchType, List<String>> returned = WordSeparator.separate(given);
+
+        //then
+        assertEquals(expected, returned);
+    }
+}
