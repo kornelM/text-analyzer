201b4b39415936367a01c3a03848699afdd04240
kornelM <makusz07@gmail.com>
2022-11-24 00:37:04 +0100

    Rearranged imports and whitespaces

--- text-analyzer-core/src/main/java/com/text/analyzer/TextAnalyzer.java
+++ text-analyzer-core/src/main/java/com/text/analyzer/TextAnalyzer.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 
 public class TextAnalyzer {
 
-
     public static void main(String[] args) throws IOException {
         PropertyLoader propertyLoader = new PropertyLoader();
 
